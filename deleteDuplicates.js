bf9ecbf8fc7e3e09eb4547014eaec6fe7f671cdb
Ashley Ren <ashleyren@BC-AREN-MBP.local>
2015-03-15 00:04:37 -0400

    hah

---
+++ deleteDuplicates.js
@@ -0,0 +1,30 @@
+/**
+* Definition for singly-linked list.
+* function ListNode(val) {
+* this.val = val;
+* this.next = null;
+* }
+*/
+
+/**
+* @param {ListNode} head
+* @return {ListNode}
+*/
+var deleteDuplicates = function(head) {
+if( head === null){
+return null;
+}
+else if(head.next === null){
+return head;
+}else{
+var x = head;
+while(x.next !== null){
+if(x.val === x.next.val){
+x.next = x.next.next;
+}else{
+x = x.next;
+}
+}
+return head;
+}
+};
